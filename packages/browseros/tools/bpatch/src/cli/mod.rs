7394cdefb86bff48d2d9f112d907bfccbb806fe2
Nikhil Sonti <nikhilsv92@gmail.com>
2026-07-07 15:18:52 -0700

    chore(bpatch): scaffold crate skeleton

---
+++ packages/browseros/tools/bpatch/src/cli/mod.rs
@@ -0,0 +1,8 @@
+pub mod abort;
+pub mod apply;
+pub mod continue_cmd;
+pub mod diff;
+pub mod extract;
+pub mod feature;
+pub mod render;
+pub mod status;
---
+++ packages/browseros/tools/bpatch/src/engine/mod.rs
@@ -0,0 +1,4 @@
+pub mod apply;
+pub mod conflict;
+pub mod extract;
+pub mod state;
